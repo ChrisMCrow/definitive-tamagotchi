796de7ed43061a82a80d2a3af6d8a63c0c831202
Chris Crow <chrismcrow@gmail.com>
2018-12-11 23:36:06 -0800

    Updates

---
+++ precache-manifest.d0719076cf9dfd9ad17d038ef8cb1fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "067536269b53532b93eaeec55cb32b25",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/static/media/tamashell.06753626.png"
+  },
+  {
+    "revision": "4241e9bdd31f982b12e8f6bee9caf1a8",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/static/media/logo.4241e9bd.png"
+  },
+  {
+    "revision": "0beb45630e5fe064dada",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/static/js/runtime~main.0beb4563.js"
+  },
+  {
+    "revision": "83d5a4bfc02ba7d3f47d",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/static/js/main.83d5a4bf.chunk.js"
+  },
+  {
+    "revision": "d8540609b9fb28aa4648",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/static/js/1.d8540609.chunk.js"
+  },
+  {
+    "revision": "d8540609b9fb28aa4648",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/static/css/1.13e06418.chunk.css"
+  },
+  {
+    "revision": "4fd7f1a7b408094d1a4065a183b80c23",
+    "url": "chrismcrow.github.io/definitive-tamagotchi/index.html"
+  }
+];
